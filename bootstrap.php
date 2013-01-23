37f8345dd3f8c698dbde40809cb9cb9a0dcecfdc
David Ogilo <david.ogilo@dayesoft.co.uk>
2013-01-23 19:37:35 +0000

    Minor bug fix to loading wp-load.php

--- bootstrap.php
+++ bootstrap.php
@@ -14,7 +14,7 @@ global $wp_rewrite, $wpdb;
 //define( 'WP_MAX_MEMORY_LIMIT', '1024M' );
 define( 'WP_MEMORY_LIMIT', '100M' );
 
-require_once( dirname( __FILE__ ) . '/../../../wp-load.php' );
+require_once( dirname( __FILE__ ) . '/../../wp-load.php' );
 require_once( ABSPATH . 'wp-admin/includes/admin.php' );
 
 if ( !PHPUNIT_BUILD_SETUP ) wp_set_current_user( 1 );
