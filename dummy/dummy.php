3d2ad53fd5892a610b80fcb4945377850fb2c2d9
David Ogilo <david.ogilo@dayesoft.co.uk>
2013-01-23 19:10:40 +0000

    Added dummy plugin Added more information to readme

---
+++ dummy/dummy.php
@@ -0,0 +1,15 @@
+<?php 
+/*
+Plugin Name: Dummy Plugin
+Plugin URI: http://www.davidogilo.co.uk
+Description: A dummy plugin to be used as an example of how to run tests using PHPUnit
+Version: 1.0
+Author: David Ogilo
+Author URI: http://www.davidogilo.co.uk
+License: GPL2
+*/
+
+require_once( dirname( __FILE__ ) . '/classes/Dummy.php' );
+
+Dummy::getInstance();
+?>
